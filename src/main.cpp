5abe8edce4294b2486eb980e728d6dfa23235d6e
Todd Vrba <todd.vrba@gmail.com>
2017-05-10 15:56:53 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <SDL.h>
+
+/**
+ * Where it all begins.
+ */
+int main(int, char**) {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0){
+    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+  SDL_Quit();
+  return 0;
+}
