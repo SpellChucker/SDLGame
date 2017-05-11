a3f18e731a011861e90161bbf6c4ff5b048ea85b
Todd Vrba <todd.vrba@gmail.com>
2017-05-11 14:39:56 -0500

    Updates
    - Add SDL2_image library.
    - Add cute penguin image.
    - Update code to display image.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Attempting to create a game in SDL2.
 
-To build, ensure you have SDL2 installed then perform the following:
+To build, ensure you have `SDL2` and `SDL2_image` installed then perform the following:
 
 ```
 $ mkdir build && cd build
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,121 @@
 #include <stdio.h>
+#include <string>
 #include <SDL.h>
+#include <SDL_image.h>
+
+std::string getResourcePath(const std::string &subDir = "") {
+#ifdef _WIN32
+  const char PATH_SEP = '\\';
+#else
+  const char PATH_SEP = '/';
+#endif
+  static std::string baseRes;
+  if (baseRes.empty()){
+    char *basePath = SDL_GetBasePath();
+    if (basePath){
+      baseRes = basePath;
+      SDL_free(basePath);
+    }
+    else {
+      printf("Error getting resource path: %s\n", SDL_GetError());
+      return "";
+    }
+    //We replace the last bin/ with res/ to get the the resource path
+    size_t pos = baseRes.rfind("bin");
+    baseRes = baseRes.substr(0, pos) + "res" + PATH_SEP;
+  }
+  return subDir.empty() ? baseRes : baseRes + subDir + PATH_SEP;
+}
+
+/**
+ * Load a texture given a file name and a renderer.
+ *
+ * @param file The image file to load
+ * @param renderer The renderer to load the texture onto
+ * @return The loaded texture or nullptr if error
+ */
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer* renderer) {
+  SDL_Texture *texture = IMG_LoadTexture(renderer, file.c_str());
+
+  if (texture == nullptr) {
+    printf("Could not load image. SDL_Error: %s\n", SDL_GetError());
+  }
+
+  return texture;
+}
+
+/**
+ * Render the texture to a renderer with a specific width, height, x, and y.
+ *
+ * @param texture The source texture we want to draw
+ * @param renderer The renderer we want to draw the texture to
+ * @param x The X coordinate to draw to
+ * @param y The Y coordinate to draw to
+ * @param width The width of the image we want to draw
+ * @param height The height of the image we want to draw
+ */
+void renderTexture(SDL_Texture* texture, SDL_Renderer* renderer, int x, int y, int width, int height) {
+  SDL_Rect destination;
+  destination.x = x;
+  destination.y = y;
+  destination.w = width;
+  destination.h = height;
+  SDL_RenderCopy(renderer, texture, NULL, &destination);
+}
 
 /**
  * Where it all begins.
  */
 int main(int, char**) {
+  // Keep a reference to our window.
   SDL_Window* window = NULL;
 
+  // Screate a screen surface which we'll use to store the window's surface.
   SDL_Surface* screenSurface = NULL;
 
+  // Create our renderer.
+  SDL_Renderer* renderer = NULL;
+
+  // Initialize SDL's video.
   if (SDL_Init(SDL_INIT_VIDEO) != 0){
-    printf("SDL could not be initialized: SDL_Error: %s\n", SDL_GetError());
+    printf("SDL could not be initialized. SDL_Error: %s\n", SDL_GetError());
     return 1;
   }
 
+  // Create our window.
   window = SDL_CreateWindow("First Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 728, SDL_WINDOW_SHOWN);
   if (window == NULL) {
     printf("Window could not be created. SDL_Error: %s\n", SDL_GetError());
     return 1;
   }
 
+  // Create our renderer.
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (renderer == NULL) {
+    SDL_DestroyWindow(window);
+    printf("Renderer could not be created. SDL_Error: %s\n", SDL_GetError());
+  }
+
+  // Store our surface.
   screenSurface = SDL_GetWindowSurface(window);
 
-  // Fill window white.
-  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  std::string imageFile = getResourcePath() + "pete_penguin.png";
+  SDL_Texture *loadedTexture = loadTexture(imageFile, renderer);
 
-  SDL_UpdateWindowSurface(window);
+  if (loadedTexture == nullptr) {
+    printf("Unable to load texture. SDL_Error: %s\n", SDL_GetError());
+    IMG_Quit();
+    SDL_Quit();
+    return 1;
+  }
+
+  renderTexture(loadedTexture, renderer, 352, 164, 320, 400);
+
+  SDL_RenderPresent(renderer);
 
   SDL_Delay(2000);
+
+  SDL_DestroyRenderer(renderer);
 
   SDL_DestroyWindow(window);
 
